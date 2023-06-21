3ede945b5391b0ec0a5c0cd47697499e527a413d
Oliver Stöhr <stoehr@effective-webwork.de>
2023-06-21 13:41:35 +0200

    Remove custom PluginManager

--- config/module.config.php
+++ config/module.config.php
@@ -4,12 +4,17 @@ namespace SearchKeys\Module\Configuration;
 $config = [
     'service_manager' => [
         'allow_override' => true,
-        'factories' => [
-            'SearchKeys\Search\Params\PluginManager' => 'VuFind\ServiceManager\AbstractPluginManagerFactory',
-            'SearchKeys\Search\SearchKeysHelper' => 'SearchKeys\Search\SearchKeysHelperFactory',
-        ],
         'aliases' => [
-            'VuFind\Search\Params\PluginManager' => 'SearchKeys\Search\Params\PluginManager',
+            'search2' => 'SearchKeys\Search\Search2\Params',
+            'solr' => 'SearchKeys\Search\Solr\Params',
+            'VuFind\Search\Solr\Params' => 'SearchKeys\Search\Solr\Params',
+        ],
+        'factories' => [
+            'VuFind\Search\Primo\Params' => 'SearchKeys\Search\Params\ParamsFactory',
+            'SearchKeys\Search\Search2\Params' => 'SearchKeys\Search\Solr\ParamsFactory',
+            'SearchKeys\Search\Solr\Params' => 'SearchKeys\Search\Solr\ParamsFactory',
+            'SearchKeys\Search\SearchKeysHelper' => 'SearchKeys\Search\SearchKeysHelperFactory',
+            'SearchKeys\View\Helper\SearchKeys\SearchBox' => 'SearchKeys\View\Helper\SearchKeys\SearchBoxFactory',
         ],
     ],
 ];
