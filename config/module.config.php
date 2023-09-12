69440424f12a87a5371868b8d413c5e8b771ac69
Johannes Schultze <schultze@effective-webwork.de>
2023-09-12 14:03:59 +0200

    VuFind 9.

--- config/module.config.php
+++ config/module.config.php
@@ -5,16 +5,29 @@ $config = [
     'service_manager' => [
         'allow_override' => true,
         'aliases' => [
-            'search2' => 'SearchKeys\Search\Search2\Params',
-            'solr' => 'SearchKeys\Search\Solr\Params',
-            'VuFind\Search\Solr\Params' => 'SearchKeys\Search\Solr\Params',
+
         ],
         'factories' => [
-            'VuFind\Search\Primo\Params' => 'SearchKeys\Search\Params\ParamsFactory',
-            'SearchKeys\Search\Search2\Params' => 'SearchKeys\Search\Solr\ParamsFactory',
-            'SearchKeys\Search\Solr\Params' => 'SearchKeys\Search\Solr\ParamsFactory',
             'SearchKeys\Search\SearchKeysHelper' => 'SearchKeys\Search\SearchKeysHelperFactory',
             'SearchKeys\View\Helper\SearchKeys\SearchBox' => 'SearchKeys\View\Helper\SearchKeys\SearchBoxFactory',
+        ],
+    ],
+
+    'vufind' => [
+        'plugin_managers' => [
+            'search_params' => [
+                'factories' => [
+                    'VuFind\Search\Primo\Params' => 'SearchKeys\Search\Params\ParamsFactory',
+                    'SearchKeys\Search\Search2\Params' => 'SearchKeys\Search\Solr\ParamsFactory',
+                    'SearchKeys\Search\Solr\Params' => 'SearchKeys\Search\Solr\ParamsFactory',
+                ],
+                'aliases' => [
+                    'search2' => 'SearchKeys\Search\Search2\Params',
+                    'solr' => 'SearchKeys\Search\Solr\Params',
+                    'VuFind\Search\Solr\Params' => 'SearchKeys\Search\Solr\Params',
+                    'VuFind\Search\Search2\Params' => 'SearchKeys\Search\Solr2\Params',
+                ],
+            ],
         ],
     ],
 ];
